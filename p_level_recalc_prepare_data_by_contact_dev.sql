446e15df41cd505a77e0b2e8d3fa15d142362bf8
NAIV <naiv@manzanagroup.ru>
2023-07-01 14:07:17 +0300

    stream with debezium in dev

---
+++ p_level_recalc_prepare_data_by_contact_dev.sql
@@ -0,0 +1,39 @@
+USE [FCDynamo_DS]
+GO
+
+/****** Object:  View [level].[contact_data]    Script Date: 26.06.2023 19:32:12 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+
+
+
+ALTER view [level].[contact_data]
+as
+select
+	[contact_id]
+ , [summ_by_current_level]
+ , [full_calculation_summ]
+ , [level_id]
+ , [level_date]
+ , [level_enddate]
+ , [level_limit]
+ , [level_keep_limit]
+ , [new_level_id]
+ , [new_level_date]
+ , [new_level_enddate] 
+ , [new_level_limit]
+ , [new_level_keep_limit]
+ , [summ_for_next_level]
+ , [change_reason]
+ , [old_level_limit]
+from
+	level.contact_data_internal with (nolock)
+where spid = @@spid;
+GO
+
+
